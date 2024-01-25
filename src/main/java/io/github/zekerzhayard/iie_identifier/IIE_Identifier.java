e53a15d5569dc57b84e5a86eca82acd052b4dc08
ZekerZhayard <trees1000@qq.com>
2024-01-25 22:01:55 +0800

    1.2

--- src/main/java/io/github/zekerzhayard/iie_identifier/IIE_Identifier.java
+++ src/main/java/io/github/zekerzhayard/iie_identifier/IIE_Identifier.java
@@ -2,8 +2,8 @@ package io.github.zekerzhayard.iie_identifier;
 
 public class IIE_Identifier {
     public static String removeIncorrectPath(String path) {
-        if (path != null && path.startsWith("create:")) {
-            return path.substring(7);
+        if (path != null && path.contains(":")) {
+            return path.split(":", 2)[1];
         }
         return path;
     }
